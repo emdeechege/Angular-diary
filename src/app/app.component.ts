3672496918196d7412f5263635ee7a715bb6654f
Tom Chege <emdeechege@gmail.com>
2018-08-08 16:41:32 +0300

    Added my new tagline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Idea} from './idea'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Diary';
+  // title = 'Diary';
 }
