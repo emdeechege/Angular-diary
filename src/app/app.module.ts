14e5466ac2362affed8edf0bd55ceb30cf9cf40c
Tom Chege <emdeechege@gmail.com>
2018-08-08 16:18:49 +0300

    created class, components and directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IdeaComponent } from './idea/idea.component';
+import { IdeaDetailsComponent } from './idea-details/idea-details.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IdeaComponent,
+    IdeaDetailsComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule
---
+++ src/app/idea-details/idea-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-idea-details',
+  templateUrl: './idea-details.component.html',
+  styleUrls: ['./idea-details.component.css']
+})
+export class IdeaDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
