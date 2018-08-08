a72bedf2820fdde34a8f14392e80b2a972851cb5
Tom Chege <emdeechege@gmail.com>
2018-08-08 16:58:27 +0300

    editing idea ts file, new idea addition, deletion

--- src/app/idea/idea.component.ts
+++ src/app/idea/idea.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Idea} from '../idea'
 @Component({
   selector: 'app-idea',
   templateUrl: './idea.component.html',
@@ -7,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IdeaComponent implements OnInit {
 
+  ideas = [
+      new Idea(1, 'Feed birds', 'Find a new spot to feed them'),
+    ]
+
+    addNewidea(idea) {
+    let ideaLength = this.ideas.length;
+    idea.id = ideaLength+1;
+    idea.completeDate = new Date(idea.completeDate)
+    this.ideas.push(idea)
+}
+
+    deleteIdea(isComplete, index) {
+      if (isComplete) {
+        let toDelete = confirm(`Are you sure you want to delete ${this.ideas[index].name}`)
+
+        if (toDelete) { this.ideas.splice(index, 1) }
+      }
+    }
+    toogleDetails(index) {
+      this.ideas[index].showDescription = !this.ideas[index].showDescription;
+    }
   constructor() { }
 
   ngOnInit() {
